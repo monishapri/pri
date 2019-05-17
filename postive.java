afa2f20056343a05b266b3ae3db95deaa3df7741
monishapri <50731910+monishapri@users.noreply.github.com>
2019-05-17 14:38:42 +0530

    Create postive.java

---
+++ postive.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+class Positive{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+System.out.println("enter the num");
+int n=scan.nextInt();
+if(n<0){
+System.out.println("the given number is negative");
+}
+else{
+System.out.println("the given number is positive");
+}
+}
+}
